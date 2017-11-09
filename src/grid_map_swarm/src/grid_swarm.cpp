31981eb506ba4b539f17dd0d15d982012bb28b0e
Christopher P <portugalchristopher96@gmail.com>
2017-11-09 15:09:02 -0800

    Adding the grid, but it doesn't change. Need to test

--- src/grid_map_swarm/src/grid_swarm.cpp
+++ src/grid_map_swarm/src/grid_swarm.cpp
@@ -1,8 +1,37 @@
 #include <ros/ros.h>
 #include <grid_map_ros/grid_map_ros.hpp>
 #include <grid_map_msgs/GridMap.h>
+#include <sensor_msgs/Range.h>
 #include <cmath>
 
-int main() {
-   return 0;
+//Global
+sensor_msgs::Range sonarLeft;
+sensor_msgs::Range sonarCenter;
+sensor_msgs::Range sonarRight;
+
+int main(int argc, char **argv) {
+  
+  gethostname(host, sizeof (host));
+  string hostname(host);
+  ros::init(argc, argv, (hostname + "_grid_map"));
+    
+  ros::NodeHandle nh("~");
+  ros::Publisher publisher = nh.advertise<grid_map_msgs::GridMap>("grid_map", 1, true);
+   
+  // Create grid map.
+  GridMap map({"elevation"});
+  map.setFrameId("map");
+  map.setGeometry(Length(3.0, 3.0), 0.05);
+  ROS_INFO("Created map with size %f x %f m (%i x %i cells).",
+    map.getLength().x(), map.getLength().y(),
+    map.getSize()(0), map.getSize()(1));  
+   
+  // Publish grid map.
+  map.setTimestamp(time.toNSec());
+  grid_map_msgs::GridMap message;
+  GridMapRosConverter::toMessage(map, message);
+  publisher.publish(message);
+  ROS_INFO_THROTTLE(1.0, "Grid map (timestamp %f) published.", message.info.header.stamp.toSec());
+   
+  return 0;
 }
