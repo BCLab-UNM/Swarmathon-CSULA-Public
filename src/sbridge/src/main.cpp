67629385fa919be72c5ca81e22ba938f96eeecd0
CPortugal <portugalchristopher96@gmail.com>
2017-11-16 18:16:17 -0800

    A->S

--- src/sbridge/src/main.cpp
+++ src/sbridge/src/main.cpp
@@ -15,7 +15,7 @@ int main(int argc, char **argv) {
     
     if (argc >= 2) {
         publishedName = argv[1];
-        cout << "Welcome to the world of tomorrow " << publishedName << "!  ABridge module started." << endl;
+        cout << "Welcome to the world of tomorrow " << publishedName << "!  SBridge module started." << endl;
     } else {
         publishedName = hostname;
         cout << "No Name Selected. Default is: " << publishedName << endl;
