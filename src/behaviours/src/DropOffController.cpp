d2934ec65ac1c5e5a82e234efae6ac28ce50e6d1
miket349 <mthong349@gmail.com>
2018-02-28 18:39:48 -0800

    changed collection tag count values and target distance for actual testing

--- src/behaviours/src/DropOffController.cpp
+++ src/behaviours/src/DropOffController.cpp
@@ -156,7 +156,7 @@ Result DropOffController::DoWork() {
 
     if (seenEnoughCenterTags) //if we have seen enough tags
         {
-          if ((countLeft-5) > countRight) //if there are too many on the left assuming the rover is inside the collection zone
+          if ((countLeft-8) > countRight) //if there are too many on the left assuming the rover is inside the collection zone
           {
               if (countRight > 0) //if there are more tags on the right
               {
@@ -167,7 +167,7 @@ Result DropOffController::DoWork() {
                   left = false; //then we say none on the right to cause us to turn right
               }
           }
-          else if ((countRight-5) > countLeft)
+          else if ((countRight-8) > countLeft)
           {
               if (countLeft > 0) //if there are more tags on the left
               {
