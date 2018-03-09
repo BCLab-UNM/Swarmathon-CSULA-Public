11383b1fcfa7a5714e79e1aceaaf22da3da94d68
miket349 <mthong349@gmail.com>
2018-03-08 16:59:10 -0800

    changed target distance for testing purpose

--- src/behaviours/src/PickUpController.cpp
+++ src/behaviours/src/PickUpController.cpp
@@ -184,8 +184,8 @@ Result PickUpController::DoWork()
   if (!targetHeld)
   {
     //threshold distance to be from the target block before attempting pickup
-    float targetDistance = 0.18; //meters
-    //float targetDistance = 0.13; //FOR SIMULATION TEST ONLY
+    //float targetDistance = 0.18; //meters
+    float targetDistance = 0.13; //FOR SIMULATION TEST ONLY
 
     // -----------------------------------------------------------
     // millisecond time = current time if not in a counting state
