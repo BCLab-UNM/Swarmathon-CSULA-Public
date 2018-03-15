b735aa9832c402ab59f58bad29bd9b8fe53b9142
Matthew Fricke <matthew@fricke.co.uk>
2018-03-15 17:12:29 -0600

    changed the rover's status

--- src/behaviours/src/ROSAdapter.cpp
+++ src/behaviours/src/ROSAdapter.cpp
@@ -647,7 +647,7 @@ void joyCmdHandler(const sensor_msgs::Joy::ConstPtr& message) {
 
 void publishStatusTimerEventHandler(const ros::TimerEvent&) {
   std_msgs::String msg;
-  msg.data = "online";		//change this with team name
+  msg.data = "Cal State LA - Not Last!!+";		//change this with team name
   status_publisher.publish(msg);
 }
 
